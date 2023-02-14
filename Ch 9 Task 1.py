4b125369b65cf09895163aa3c02ff1e45bbab095
s560274@nwmissouri.edu <you@example.com>
2023-02-13 20:44:37 -0600

    ch 9

--- Ch 9 Task 1.py
+++ Ch 9 Task 1.py
@@ -135,3 +135,22 @@ def function1():
 def function2(): 
     raise Exception('An exception occurred')
 
+import csv
+
+with open ('accounts.csv', mode='w', newline= ' ') as accounts: 
+    writer = csv.writer(accounts)
+    writer.writerow([100, 'Jones', 24.98])
+    writer.writerow([200, 'Doe', 345.67])
+    writer.writerow([300, 'White', 0.00])
+    writer.writerow([400, 'Stone', -42.16])
+    writer.writerow([500, 'Rich', 224.62])
+
+with open('accounts.csv', 'r', newline= '') as accounts: 
+    print(f'{"Account":<10}{"Name":<10}{"Balance:>10"}')
+    reader = csv.reader(accounts)
+    for record in reader:
+        account, name, balance = record 
+        print(f'{account:<10}{name:<10}{balance:>10}')
+
+
+
